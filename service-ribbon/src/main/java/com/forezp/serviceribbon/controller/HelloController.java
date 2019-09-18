02dcde1a21678aec4ea053bec17b1a7120ca0805
zhoumiaode <1010467470@qq.com>
2019-09-18 11:05:03 +0800

    first

---
+++ service-ribbon/src/main/java/com/forezp/serviceribbon/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.forezp.serviceribbon.controller;
+
+import com.forezp.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author zmd
+ * @Date 2019/9/18 9:27
+ * Version 1.0
+ **/
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+
+    @GetMapping(value = "his")
+    public String his(){
+        return "123";
+    }
+}
---
+++ service-ribbon/src/main/java/com/forezp/serviceribbon/service/HelloService.java
@@ -0,0 +1,24 @@
+package com.forezp.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName HelloService
+ * @Description TODO
+ * @Author zmd
+ * @Date 2019/9/18 9:26
+ * Version 1.0
+ **/
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+}
